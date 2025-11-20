43bacf1a73087c2bec4457b99645cc6b82d9cc0d
JACK-Prime <natthakitrodruean@gmail.com>
2025-11-20 22:43:34 +0700

    second commit

---
+++ LAB1/LAB1p12.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class LAB1p12 {
+    public static void main (String[] agrs) {
+        Scanner input = new Scanner(System.in);
+
+        input.close();
+    }
+}
--- LAB1/LAB1p9.java
+++ LAB1/LAB1p9.java
@@ -2,30 +2,30 @@ import java.util.Scanner;
 
 public class LAB1p9 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in) ;
         int N ;
         int Sum = 0 ;
         double AVG = 0.0f ;
 
         // Input score 
-        System.out.print("Enter N: "); //จำนวนครั้ง
-        N = input.nextInt();
-        int[] Array = new int[N];
+        System.out.print("Enter N: ") ; //จำนวนครั้ง
+        N = input.nextInt() ;
+        int[] Array = new int[N] ;
 
         // Process
         for (int i = 0; i < N ; i++) {
-            System.out.printf("%s %d%s" ,"Enter Number" , i+1 ,": "  ); 
-            Array[i] = input.nextInt();
+            System.out.printf("%s %d%s" ,"Enter Number" , i+1 ,": "  ) ; 
+            Array[i] = input.nextInt() ;
             Sum += Array[i] ;
         }
         // cal
         // (ต้องแปลง sum เป็น double "ก่อน" หาร เพื่อให้ได้ทศนิยม)
-        AVG = (double) Sum / Array.length;
+        AVG = (double) Sum / Array.length ;
 
         // Output
-        System.out.println("\nThe Average is : " + AVG );
+        System.out.println("\nThe Average is : " + AVG ) ;
 
         // Close scanner 
-        input.close();
-    }
+        input.close() ;
+    } 
 }
