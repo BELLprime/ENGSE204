b52fd3296be0d4c3f22321513c90847a51c77749
JACK-Prime <natthakitrodruean@gmail.com>
2025-11-22 10:57:43 +0700

    update LAB1p14 4/6

--- LAB1/LAB1p14.java
+++ LAB1/LAB1p14.java
@@ -1,75 +1,87 @@
-import java.util.Scanner;
+// NO code form AI 100% code by natthakit
+import java.util.Scanner ;
 
 public class LAB1p14 {
-    public static void main (String[] args) {
-        Scanner input = new Scanner(System.in) ;
+
+    public static void main ( String [ ] args) {
+
+        Scanner input = new Scanner( System.in ) ;
 
         int Row , Col ;
+
         int bomb = 0 ;
 
-        // Input
-        System.out.print("Enter (rows,columns): ");
-        Row = input.nextInt();
-        Col = input.nextInt();
-        System.out.println("----- MAP " + Row + "x" + Col + " -----" );
-        char[][] dashboard =  new char[Row][Col] ;
+        System.out.print("MAP: " ); 
 
-        // Input MAP and target
-        for ( int i = 0 ; i < dashboard.length ; i ++ ) { // rows
-            for ( int j = 0 ; j < dashboard[i].length ; j++  ) {  // column
-                    dashboard[i][j] = input.next().charAt(0); 
+        Row = input.nextInt () ;
+        Col = input.nextInt () ;
+
+        char[][] MAP =  new char[Row][Col] ;
+
+        
+        for ( int i = 0 ; i < MAP.length ; i ++ ) {
+            for ( int j = 0 ; j < MAP[i].length ; j++  ) {  
+                    MAP[ i ] [ j ] = input.next().charAt(0) ; 
             }
         }  
-        System.out.print("Enter target(row , column): ") ; // -----เก็บ target--------
+        System.out.print("Target :") ; 
         int TRow = input.nextInt() ;
-        int TCol = input.nextInt() ; // --------------------------------------------------------
+        int TCol = input.nextInt() ;
         
-        // Process and Show screen
-        boolean Minebomb = false;
+        int Minebomb = 0 ;
 
-        // check, is target over MAP? 
         if (TRow >= 0 && TRow < Row && TCol >= 0 && TCol < Col) {
-            if (dashboard[ TRow ][ TCol ] == '*') {
-                Minebomb = true ;
+            if (MAP[ TRow ][ TCol ] == '*') {
+                Minebomb = 1 ;
             }
         }
-        if (Minebomb) {
+        if (Minebomb == 1) {
             System.out.println("Mine");
         } 
-        else { //count mine near by.
-            bomb = 0;
-           /* 
-            * * *   1 2 3 
-            * x *   4 x 5
-            * * *   6 7 8
-                          */
+        else { 
+            bomb = 0 ;
             if ( TRow - 1 >= 0 && TCol - 1 >= 0 ) {
-                if (dashboard[ TRow - 1 ] [ TCol - 1 ] == '*') bomb++; //บนซ้าย
+                if (MAP[ TRow - 1 ] [ TCol - 1 ] == '*') {
+                    bomb++; //above L
+                }
             }
             if ( TRow - 1 >= 0 && TCol >= 0 && TCol < Col ) {
-                if (dashboard[ TRow - 1 ] [ TCol ] == '*') bomb++; //บนกลาง
+                if (MAP[ TRow - 1 ] [ TCol ] == '*') {
+                    bomb++; //above M
+                }
             }
             if ( TRow - 1 >= 0 && TCol + 1 < Col ) {
-                if (dashboard[ TRow - 1 ] [ TCol + 1 ] == '*') bomb++; //บนขวา
+                if (MAP[ TRow - 1 ] [ TCol + 1 ] == '*') {
+                    bomb++; //above R
+                }
             }
             if ( TRow - 1 >= 0 && TCol + 1 < Col ) {
-                if (dashboard[ TRow ] [ TCol - 1 ] == '*') bomb++; //ซ้าย
+                if (MAP[ TRow ] [ TCol - 1 ] == '*') {
+                    bomb++; //L
+                }
             }
             if ( TRow >= 0 && TRow < Row && TCol + 1 < Col ) {
-                if (dashboard[ TRow ] [ TCol + 1 ] == '*') bomb++; //ขวา
+                if (MAP[ TRow ] [ TCol + 1 ] == '*') {
+                    bomb++; //R
+                }
             }
             if ( TRow + 1 < Row && TCol - 1 >= 0 ) {
-                if (dashboard[ TRow + 1 ] [ TCol - 1 ] == '*') bomb++; //ล่างซ้าย
+                if (MAP[ TRow + 1 ] [ TCol - 1 ] == '*') {
+                    bomb++; //down L
+                }
             }
             if ( TRow + 1 < Row && TCol >= 0 && TCol < Col ) {
-                if (dashboard[ TRow + 1] [ TCol ] == '*') bomb++; //ล่างกลาง
+                if (MAP[ TRow + 1] [ TCol ] == '*') {
+                    bomb++; //down M
+                }
             }
             if ( TRow + 1 < Row && TCol + 1 < Col ) {
-                if (dashboard[ TRow + 1 ] [ TCol + 1 ] == '*') bomb++; //ล่างขวา
+                if (MAP[ TRow + 1 ] [ TCol + 1 ] == '*') {
+                    bomb++; //down R
+                }
             }
             System.out.println(bomb); 
         }
-        
 
         input.close() ;   
     }
