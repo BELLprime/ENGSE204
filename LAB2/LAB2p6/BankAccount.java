1d429eb66d6dbe4706ffa76c598c22b7e7470f2f
JACK-Prime <natthakitrodruean@gmail.com>
2025-11-27 14:47:31 +0700

    update LAB2p6

--- LAB2/LAB2p6/BankAccount.java
+++ LAB2/LAB2p6/BankAccount.java
@@ -4,17 +4,16 @@ public class BankAccount {
     private String ownerName ;
     private double balance ;
 
-    //construc
+
     public BankAccount (String  ownerName , double initialBalance ) {
         this.ownerName = ownerName ;
         this.balance = initialBalance ;
     }
 
-    //met
-    public void deposit (double amount) {
+    public void deposit ( double amount ) {
         this.balance += amount ;
     }
-    //show
+
     public void displaySummary () {
         System.out.println("Owner: " + this.ownerName ) ;
         System.out.println("Balance: " + this.balance ) ;
--- LAB2/LAB2p6/Main.java
+++ LAB2/LAB2p6/Main.java
@@ -1,25 +1,19 @@
 package LAB2.LAB2p6;
 
 import java.util.Scanner; 
-
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String [ ] args) {
         Scanner input = new Scanner(System.in) ;
 
-        System.out.print("Enter Name : ") ; 
         String owner = input.nextLine() ; 
-        System.out.print("Enter initialBalance : ") ; 
         double initial = input.nextDouble() ; 
-        System.out.print("Enter Deposit amount : ") ;
         double deposit = input.nextDouble() ; 
 
-        BankAccount Bank = new BankAccount(owner, initial) ;
+        BankAccount Bank = new BankAccount(owner , initial) ;
         Bank.deposit(deposit) ;
-        //display
         Bank.displaySummary();
 
         input.close() ;
-
     }
 }
 
