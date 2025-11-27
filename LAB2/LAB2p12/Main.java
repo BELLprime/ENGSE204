6e9cfe88b9834f8915ac6622641824a4fb7cb6ed
JACK-Prime <natthakitrodruean@gmail.com>
2025-11-27 11:33:58 +0700

    update LAB2p12

--- LAB2/LAB2p12/Main.java
+++ LAB2/LAB2p12/Main.java
@@ -6,11 +6,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in) ;
 
-        System.out.print("Course code : ") ;
         String courseId = input.nextLine() ; 
-        System.out.print("Course name : ") ;
+        
         String courseName = input.nextLine() ;
-        System.out.print("Student name : ") ;
+        
         String studentName = input.nextLine() ;
 
         Course course = new Course( courseId , courseName ) ;
