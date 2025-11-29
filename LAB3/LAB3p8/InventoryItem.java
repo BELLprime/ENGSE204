c27136fc519cdaf9cee2bb9e9cff549297b6f2d0
JACK-Prime <natthakitrodruean@gmail.com>
2025-11-29 22:21:48 +0700

    upload LAB3p8

---
+++ LAB3/LAB3p8/InventoryItem.java
@@ -0,0 +1,61 @@
+package LAB3.LAB3p8;
+
+public class InventoryItem {
+    
+    private String productName ;
+    private int stock ;
+
+    public InventoryItem(String productName , int initialStock ) {
+        
+        this.productName = productName;
+
+        if ( initialStock >= 0 ) this.stock = initialStock ;  
+        else this.stock = 0 ;
+
+    }
+
+    public String getProductName() {
+
+        return this.productName ;
+
+    }
+
+    public int getStock() {
+
+        return this.stock ;
+
+    }
+
+    public void addStock (int amount ) {
+
+        if (amount > 0) {
+
+            this.stock += amount ;
+
+        } else {
+
+            throw new IllegalArgumentException("Invalid amount.") ;
+
+        }
+        
+    }
+    
+    public void sellStock(int amount ) {
+        
+        if (amount > 0 && amount <= this.stock) {
+
+            this.stock -= amount ;
+
+        } else if (amount > this.stock ) {
+
+            throw new IllegalArgumentException("Not enough stock.") ;
+
+        } else {
+
+            throw new IllegalArgumentException("Invalid amount.") ;
+
+        }
+        
+    }
+
+}
---
+++ LAB3/LAB3p8/Main.java
@@ -0,0 +1,71 @@
+package LAB3.LAB3p8;
+
+import java.util.Scanner ; 
+import java.util.ArrayList ;
+
+public class Main {
+    public static void main( String [ ] args ) {
+
+        Scanner input = new Scanner(System.in) ;
+        
+        ArrayList <String> listNotify = new ArrayList <> () ;
+
+        String productName = input.nextLine() ;
+        int initStock = input.nextInt() ;
+        
+        InventoryItem inventoryItem = new InventoryItem(productName , initStock) ;
+        
+        int N = input.nextInt() ;
+
+        for (int i = 0 ; i < N ; i++) {
+
+            input.nextLine() ;
+            String mode = input.nextLine().trim() ;
+
+            if (mode.equals("ADD") ) {
+
+                int amount = input.nextInt() ;
+
+                try {
+
+                    inventoryItem.addStock(amount) ;
+                    listNotify.add("Stock added.") ;
+
+                } catch ( IllegalArgumentException e ) {
+
+                    System.out.println(  ) ;
+                    listNotify.add( e.getMessage() ) ;
+
+                }
+
+            } else if (mode.equals("SELL") ) {
+
+                int amount = input.nextInt() ;
+
+                try {
+
+                    inventoryItem.sellStock(amount) ;
+                    listNotify.add( "Sale successful." ) ;
+
+                } catch ( IllegalArgumentException e ) {
+
+                    listNotify.add( e.getMessage() ) ;
+
+                }
+            } else {
+
+                System.out.println( "Invalid Mode." ) ;
+
+            }
+        } 
+
+        for  (int i = 0 ; i < listNotify.size() ; i ++ ) {
+            
+            System.out.println( listNotify.get( i ) ) ;
+
+        }
+        System.out.println( "Final Stock: " + inventoryItem.getStock() ) ;
+
+        input.close () ; 
+    }
+}
