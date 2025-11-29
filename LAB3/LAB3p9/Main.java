7091c084a873c204d54a569be8f4445c48e17c27
JACK-Prime <natthakitrodruean@gmail.com>
2025-11-30 00:29:34 +0700

    upload LAB3p9

--- LAB3/LAB3p9/Main.java
+++ LAB3/LAB3p9/Main.java
@@ -1,5 +1,48 @@
 package LAB3.LAB3p9;
 
+import java.util.Scanner ; 
+
 public class Main {
-    
+    public static void main( String [ ] args ) {
+
+        Scanner input = new Scanner(System.in) ;
+
+        int minLength1 = input.nextInt() ;
+        input.nextLine();
+        String user1_name = input.nextLine() ;
+        String user1_pass = input.nextLine() ;
+        String user2_name = input.nextLine() ;
+        String user2_pass = input.nextLine() ;
+        int minLength2 = input.nextInt() ;
+        input.nextLine();
+        String user2_newPass = input.nextLine() ;
+
+
+        try {
+            User.setMinLength( minLength1 ) ;
+        } catch ( IllegalArgumentException e ) {
+            System.out.println( e.getMessage() ) ;
+        }
+
+        User user1 = new User( user1_name, user1_pass ) ;
+        
+        User user2 = new User( user2_name, user2_pass ) ;
+
+        try {
+            User.setMinLength( minLength2 ) ;
+        } catch ( IllegalArgumentException e ) {
+            System.out.println( e.getMessage() ) ;
+        }
+
+        try {
+            user2.setPassword(user2_newPass) ;
+        } catch ( IllegalArgumentException e ) {
+            System.out.println( e.getMessage() ) ;
+        }
+        
+        System.out.println( user1.getPassword() ) ;
+        System.out.println( user2.getPassword() ) ;
+
+        input.close () ; 
+    }
 }
---
+++ LAB3/LAB3p9/User.java
@@ -0,0 +1,63 @@
+package LAB3.LAB3p9;
+
+class User {
+
+    private String username ;
+    private String password ;
+
+    private static int minPasswordLength = 8 ;
+
+    public User ( String username , String password) {
+        if ( password.trim().length() >= minPasswordLength ) {
+
+            this.password = password ;
+            System.out.println("Creation successful." ) ;
+
+        }  else {
+            
+            this.password = "invalid" ;
+            System.out.println( "Creation failed." ) ;
+
+        }
+
+    }
+    public String getPassword() {
+        
+        return this.password ;
+    }
+
+    public String getName() {
+        
+        return this.username ;
+    }
+
+    public void setPassword(String newPassword) {
+        if ( newPassword.length() >= minPasswordLength ) {
+
+            this.password = newPassword ;
+            System.out.println("Update successful." ) ;
+
+        }  else {
+            
+            throw new IllegalArgumentException( "Update failed." ) ;
+
+        }
+    }
+
+    public static void setMinLength (int length) {
+        if ( length >= 4 ) {
+
+            minPasswordLength = length ;
+            System.out.println("New min length set to " + length) ;
+            
+        } else {
+
+            throw new IllegalArgumentException("Invalid length.") ;
+        }
+    }
+    
+    public static int getMinLength() {
+
+        return minPasswordLength ;
+    }
+}
