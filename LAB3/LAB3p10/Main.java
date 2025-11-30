9c8e04310c9f79cbe2ca67612d40f33995f24ac9
JACK-Prime <natthakitrodruean@gmail.com>
2025-11-30 16:31:05 +0700

    modified LAb3p10

--- LAB3/LAB3p10/Main.java
+++ LAB3/LAB3p10/Main.java
@@ -8,18 +8,15 @@ public class Main {
 
         Scanner input = new Scanner(System.in) ;
 
-        System.out.print("Enter N : ") ; ///
         int N = input.nextInt() ;
         input.nextLine() ;
 
         for (int i = 0 ; i < N ; i++ ) {
             
-            System.out.print("Enter Order : ") ;///
             String order = input.nextLine() ;
             
             if ( order.trim().toUpperCase().equals("SET") ) {
-                
-                System.out.print("Enter Log : ") ;
+
                 int Log = input.nextInt() ;
 
                 input.nextLine() ;
@@ -33,12 +30,10 @@ public class Main {
             } 
             else if ( order.trim().toUpperCase().equals("LOG") ) {
 
-                System.out.print("Enter Log : ") ;
                 int Log = input.nextInt() ;
 
                 input.nextLine() ;
 
-                System.out.print("Enter message : ") ;
                 String message = input.nextLine() ;
                 SystemLogger.log(Log, message) ;
             }
