d1be6ba84678124aecb709642211c03d99b75f47
JACK-Prime <natthakitrodruean@gmail.com>
2025-12-04 18:05:24 +0700

    update LAB3p13

--- LAB3/LAB3p13/LicenseManager.java
+++ LAB3/LAB3p13/LicenseManager.java
@@ -4,58 +4,38 @@ class LicenseManager {
 
     private static int maxLicenses = 10 ;
     private static int usedLicenses = 0 ;
-
+    
     public static void setMax(int max) {
-
         if ( max < 0 ) {
-
             throw new IllegalArgumentException("invalid max value") ;
         }
         else if ( max < usedLicenses ) {
-
             throw new IllegalArgumentException("Cannot Set max lower than current usage.") ;
         } 
         else {
-             
             maxLicenses = max ;
-
             System.out.println("Maxlicense set to " + max + ".") ;
         }
-
     }
-
     public static boolean checkOut() {
-
         if (usedLicenses < maxLicenses) {
-
-            usedLicenses++ ;
-            
+            usedLicenses++ ;   
             System.out.println("Checkout successful.") ;
-
             return true ;
         }
         else {
-
             System.out.println("Checkout failed: No license available.") ;
-
             return false ;
         }
     }
-
     public static void checkIn() {
-
         if ( usedLicenses > 0 ) {
-
             usedLicenses-- ; 
-
             System.out.println("Check-in successful.") ;
         } 
         else System.out.println("Nothing to check-in.") ;
-
     }
-
     public static void displayStatus() {
-
         System.out.println("Used: " + usedLicenses) ;
         System.out.println("Available: " + (maxLicenses - usedLicenses) ) ;
     }
--- LAB3/LAB3p13/Main.java
+++ LAB3/LAB3p13/Main.java
@@ -8,38 +8,27 @@ public class Main {
         Scanner input = new Scanner(System.in) ;
         
         int N = input.nextInt() ;
-        input.nextLine() ;
-
+        input.nextLine() ; //clear nextline
         for (int i = 0 ; i < N ; i ++ ) {
-
             String mode = input.nextLine() ;
-
-            if ( mode.trim().toUpperCase().equals("SET") )  {
-                
+            if ( mode.trim().toUpperCase().equals("SET") )  {               
                 int max = input.nextInt() ;
                 input.nextLine() ;
-
                 try {
                     LicenseManager.setMax( max ) ;
                 } catch (IllegalArgumentException e) {
                     System.out.println( e.getMessage() ) ;
-                }
-                
-            } else if ( mode.trim().toUpperCase().equals("CHECKOUT" ) ) {
-                
+                }                
+            } else if ( mode.trim().toUpperCase().equals("CHECKOUT" ) ) {              
                 LicenseManager.checkOut() ;
 
             } else if ( mode.trim().toUpperCase().equals("CHECKIN") )  {
-                
                 LicenseManager.checkIn() ; 
 
-            } else if ( mode.trim().toUpperCase().equals("STATUS") )  {
-                
+            } else if ( mode.trim().toUpperCase().equals("STATUS") )  {               
                 LicenseManager.displayStatus() ;
             }
-
         }
-
-        input.close () ; 
+        input.close () ; //close scanner 
     }
 }
