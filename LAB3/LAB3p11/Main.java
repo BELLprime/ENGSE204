e51e432ba635af9360ccee57e2d6d3edbd8f3867
JACK-Prime <natthakitrodruean@gmail.com>
2025-12-04 17:29:35 +0700

    fixed throe error  LAB3p10

--- LAB3/LAB3p11/Main.java
+++ LAB3/LAB3p11/Main.java
@@ -1,46 +1,43 @@
 package LAB3.LAB3p11;
 
 import java.util.Scanner ; 
-
 public class Main {
     public static void main( String [ ] args ) {
-
         Scanner input = new Scanner(System.in) ;
         
         int N = input.nextInt() ;
         input.nextLine () ; 
 
         for (int i = 0 ; i < N ; i++ ) {
-            
+
             String mode = input.nextLine() ;
 
             if ( mode.trim().toUpperCase().equals("SET_URL") )  {
-                
                 String URL = input.nextLine() ;
-                SystemConfig config = SystemConfig.getInstance() ;
-                config.setServerUrl(URL) ;
-
-            } else if ( mode.trim().toUpperCase().equals("SET_MAX" ) ) {
-                
+                SystemConfig config = SystemConfig.getInstance() ; //use static getInstance 
+                try {
+                     config.setServerUrl( URL ) ; ;
+                } catch (IllegalArgumentException e) {
+                    System.out.println( e.getMessage() ) ; 
+                }
+            } 
+            else if ( mode.trim().toUpperCase().equals("SET_MAX" ) ) {
                 int Max_Count = input.nextInt() ;
-                input.nextLine () ; 
+                input.nextLine () ; //clear buff 
                 SystemConfig config = SystemConfig.getInstance() ;
-                
+                //protect error
                 try {
                     config.setMaxConnections( Max_Count ) ;
                 } catch (IllegalArgumentException e) {
                     System.out.println( e.getMessage() ) ; 
                 }
-            
+        
             } else if ( mode.trim().toUpperCase().equals("SHOW") )  {
-                
-                SystemConfig config = SystemConfig.getInstance() ;
+                SystemConfig config = SystemConfig.getInstance() ;//use static
                 System.out.println("URL: " + config.getServerUrl() ) ;
                 System.out.println("MAX: " + config.getMaxConnections()) ;
             }
-
         }
-
         input.close () ; 
     }
 }
--- LAB3/LAB3p11/SystemComfig.java
+++ LAB3/LAB3p11/SystemComfig.java
@@ -3,50 +3,43 @@ package LAB3.LAB3p11;
 class SystemConfig {
 
     private static SystemConfig instance ;
-
-
     private String serverUrl ;
     private int maxConnections ;
-
-
+    //con
     private SystemConfig() {
-
         this.serverUrl = "default.sever.com" ;
         this.maxConnections = 10 ;
-
     }
-
+    //static
     public static SystemConfig getInstance() {
-
         if (instance == null ){
             instance = new SystemConfig() ;
         }
         return instance ;
-
     }
 
     public String getServerUrl() {
-
         return this.serverUrl ;
     }
 
     public void setServerUrl (String url) {
-
+        if (url == null || url.isEmpty() ) {
+            throw new IllegalArgumentException("ServerURL cannot be null or empty") ;
+        }
         this.serverUrl = url ;
     }
 
     public int getMaxConnections() {
-
         return this.maxConnections ;
     }
 
     public void setMaxConnections (int count) {
-
         if (count > 0 ) {
             this.maxConnections = count ;
             System.out.println("Max connections set.") ;
-        } 
-        else throw new IllegalArgumentException("Invalid count.") ;
+        } else {   
+            throw new IllegalArgumentException("Invalid count.") ;
+        }
     
     }
 }
