fa8718b29ade4fcc6e44efce0eb3ca8b02d472bf
JACK-Prime <natthakitrodruean@gmail.com>
2025-12-04 18:33:25 +0700

    update LAB3p15

--- LAB3/LAB3p15/Main.java
+++ LAB3/LAB3p15/Main.java
@@ -1,41 +1,30 @@
 package LAB3.LAB3p15;
 
 import java.util.Scanner ; 
-
 public class Main {
     public static void main( String [ ] args ) {
 
         Scanner input = new Scanner(System.in) ;
-        
         String username = input.nextLine() ;
-
         User user = new User( username ) ;
-
         int N = input.nextInt() ;
-        input.nextLine() ;
-
+        input.nextLine() ;//clear buff
         for ( int i = 0 ; i < N ; i++ ) {
-
             String mode = input.nextLine() ;
             if ( mode.trim().toUpperCase().equals("SET_POLICY" ) ) {
-
                 int max = input.nextInt() ;
                 input.nextLine () ;
-
+                //protect error
                 try {
                     User.setPolicy( max ) ;
                 } catch (IllegalArgumentException e) {
                     System.out.println( e.getMessage() ) ;
                 }
-
             } else if ( mode.trim().toUpperCase().equals("LOGIN" ) ) {
-
                 String password = input.nextLine() ;
                 user.login(password) ;
-
             }
         }
-
-        input.close () ; 
+        input.close () ; //close scanner 
     }
 }
--- LAB3/LAB3p15/User.java
+++ LAB3/LAB3p15/User.java
@@ -1,77 +1,47 @@
 package LAB3.LAB3p15;
-
 class User {
-
     private String username ;
     private int failedAttempts ;
     private boolean isLocked ;
 
-    private static int maxAttempts = 3 ;
-
-
+    private static int maxAttempts = 3 ; //start
+    //construct
     public User (String username) {
-
         this.username = username ;
-
         this.failedAttempts = 0 ; 
-
         this.isLocked = false ;
     }
-
-    public String getUsername() { 
-        
+    public String getUsername() { //read 
         return this.username ; 
     }
-
-    public boolean isLocked() { 
-        
+    public boolean isLocked() {  //read 
         return this.isLocked ; 
     }
-
-    public int getFailedAttempts() { 
-        
+    public int getFailedAttempts() {  //read 
         return this.failedAttempts ; 
     }
-
-
-    public static void setPolicy (int max) {
+    public static void setPolicy (int max) { //throw error
         if ( max > 0 ) {
-
             maxAttempts = max ; 
-
             System.out.println("Policy updated.") ;
-
-        } else throw new IllegalArgumentException("Invalid policy.") ;
-
+        } else {
+            throw new IllegalArgumentException("Invalid policy.") ;
+        } 
     }
-
-
     public void login(String password) {
-        
         if ( this.isLocked == true ) {
-
             System.out.println("Account is locked.") ;
-
-        } else if (this.isLocked == false && password.equals("pass123") )  {
-
+        } else if (this.isLocked == false && password.equals("pass123") )  { //if used password is pass123 
             this.failedAttempts = 0 ;
             System.out.println("Login successful.") ;
-
-        } else if (this.isLocked == false && !password.equals("pass123") ) {
-
-            this.failedAttempts++ ;
-
+        } else if (this.isLocked == false && !password.equals("pass123") ) { //if used password is pass123 
+            this.failedAttempts++ ; //+1
             if ( this.failedAttempts >= maxAttempts ) {
-
                 this.isLocked = true ;
                 System.out.println("Login failed. Account locked.") ;
-
             } else {
-
-                System.out.println("Login failed. " + (maxAttempts - this.failedAttempts) + " attempts left." ) ;
-
+                System.out.println("Login failed. " + (maxAttempts - this.failedAttempts) + " attempts left." ) ; //failed
             }
-
         }
     }
 }
