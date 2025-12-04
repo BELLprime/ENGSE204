9844b2e5d7a346e74dcedb83f312bf6949933004
JACK-Prime <natthakitrodruean@gmail.com>
2025-12-04 18:20:14 +0700

    update LAB3p14

--- LAB3/LAB3p14/BankAccount.java
+++ LAB3/LAB3p14/BankAccount.java
@@ -3,61 +3,36 @@ package LAB3.LAB3p14;
 class BankAccount {
  
     private double balance;
-
     private static int totalTransactionCount = 0;
-
+    //construct
     public BankAccount (double initialDeposit) {
-
         if ( initialDeposit >= 0 ) {
-
             this.balance = initialDeposit ; 
-
         } else this.balance = 0 ;
-
         System.out.println("Account created.") ;
     }
-
     public double getBalance() {
-    
         return this.balance ;
-
     }
-
     public void deposit (double amount) {
-
         if ( amount > 0 ) {
-
             this.balance += amount ;
-        
             totalTransactionCount++ ;
-        
             System.out.println("Deposit successful.") ;
-
         } else throw new IllegalArgumentException ("Invalid amount.") ;
     }
-
     public void withdraw(double amount) {
-
         if ( amount > 0 && amount <= this.balance ) {
-
             this.balance -= amount ;
-
             totalTransactionCount++ ;
-
             System.out.println("Withdrawal successful.") ;
-
         } else if (amount > this.balance) {
-
             throw new IllegalArgumentException ("Insufficient funds.") ;
-
-        } else throw new IllegalArgumentException ("Invalid amount.") ;
-
-
+        } else {
+            throw new IllegalArgumentException ("Invalid amount.") ;
+        }
     }
-
     public static int getTotalTransactionCount() {
-
         return totalTransactionCount ;
-
     }
 }
--- LAB3/LAB3p14/Main.java
+++ LAB3/LAB3p14/Main.java
@@ -1,78 +1,55 @@
 package LAB3.LAB3p14;
 
 import java.util.Scanner ; 
-
 public class Main {
     public static void main( String [ ] args ) {
 
         Scanner input = new Scanner(System.in) ;
-
         BankAccount myAccount = null ;
 
         int N = input.nextInt() ;
         input.nextLine() ;
 
         for ( int i = 0 ; i < N ; i++ ) {
-
             String mode = input.nextLine() ;
 
             if ( mode.trim().toUpperCase().equals("CREATE") )  {
-                
                 double initialBalance = input.nextDouble() ;
                 input.nextLine() ;
                 myAccount = new BankAccount( initialBalance ) ;
-
             } else if ( mode.trim().toUpperCase().equals("DEPOSIT") || mode.trim().toUpperCase().equals("WITHDRAW")) {
-                
                 double amount = input.nextDouble() ;
                 input.nextLine() ;
-
+                //protect error
                 if ( myAccount == null )  {
-
                     System.out.println("No account exists.") ;
-
                 } else {
-                    
                     if ( mode.trim().toUpperCase().equals("DEPOSIT") ){
-
                         try {
                             myAccount.deposit( amount ) ;
                         } catch (IllegalArgumentException e ) {
                             System.out.println( e.getMessage() ) ;
                         }
-                        
                     } 
                     else if ( mode.trim().toUpperCase().equals("WITHDRAW") ) {
-                        
                         try {
                             myAccount.withdraw( amount ) ;
                         } catch (IllegalArgumentException e) {
                             System.out.println( e.getMessage() ) ;
                         }
-                        
                     }
                 }
-
             } else if ( mode.trim().toUpperCase().equals("STATUS") )  {
-                
                 if ( myAccount != null )  {
-
                     System.out.println("Balance: " + myAccount.getBalance() ) ;
-
                 } else {
-
                     System.out.println("No account exists." ) ;
-
                 }
-
-            } else if ( mode.trim().toUpperCase().equals("GLOBAL_STATUS") )  {
-                
+            } else if ( mode.trim().toUpperCase().equals("GLOBAL_STATUS") )  {              
                 System.out.println("Total Transactions: " + BankAccount.getTotalTransactionCount() ) ;
             }
-
         }
-
-        input.close () ; 
+        input.close () ; //close scanner
     }
 }
 
